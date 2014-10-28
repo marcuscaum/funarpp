6d8c7bf7eeb0bf24c2ce8ad1cf51c4393ecca3c8
marcuscaum <marcuslucas.jb@gmail.com>
2014-10-28 09:37:20 -0200

    Ajustes

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -5,7 +5,7 @@ angular.module('starter.services', [])
         var networkState = navigator.connection.type;
         var states = {};
         var msg = ''; 
-        
+
         states[Connection.UNKNOWN]  = 'Unknown connection';
         states[Connection.ETHERNET] = 'Ethernet connection';
         states[Connection.WIFI]     = 'WiFi connection';
@@ -14,7 +14,7 @@ angular.module('starter.services', [])
         states[Connection.CELL_4G]  = 'Cell 4G connection';
         states[Connection.CELL]     = 'Cell generic connection';
         states[Connection.NONE]     = 'No network connection';
-        
+
         if(states[networkState] == states[Connection.NONE])
             msg = " Você não está conectado a nenhuma rede";
 
@@ -25,7 +25,7 @@ angular.module('starter.services', [])
   
     this.showAlert = function(template){
         var alertPopup = $ionicPopup.alert({
-            template:template
+          template:template
         })
     }
 
@@ -35,7 +35,6 @@ angular.module('starter.services', [])
 
     this.changeMsgSlide = function(){
         var valueSlide = $ionicSlideBoxDelegate.currentIndex();
-
         if (valueSlide == 0) {
             msg = 'Buscas Recentes'
         }else if(valueSlide == 1){
